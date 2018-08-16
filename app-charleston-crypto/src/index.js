bf28e74ef5567b158a8269fc8f7595afcb0732c5
Brian Liotti <bdliotti@gmail.com>
2018-08-16 19:37:49 -0400

    service worker

--- app-charleston-crypto/src/index.js
+++ app-charleston-crypto/src/index.js
@@ -5,6 +5,7 @@ import App from "./App"
 import "typeface-roboto"
 import store from "./store"
 import { Provider } from "react-redux"
+import registerServiceWorker from "./registerServiceWorker"
 import { setCategories } from "./action-creators/categories"
 import { fetchResources } from "./action-creators/resources"
 
@@ -15,5 +16,6 @@ ReactDOM.render(
   document.getElementById("root")
 )
 
+registerServiceWorker()
 store.dispatch(setCategories)
 store.dispatch(fetchResources)
