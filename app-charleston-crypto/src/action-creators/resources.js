a9749d265ac54c467a654f47fba79844fb0e8450
Brian Liotti <bdliotti@gmail.com>
2018-08-16 22:52:07 -0400

    set resources in localStorage

--- app-charleston-crypto/src/action-creators/resources.js
+++ app-charleston-crypto/src/action-creators/resources.js
@@ -1,4 +1,5 @@
 import { GET_RESOURCES } from "../constants"
+import { isNil } from "ramda"
 const scrapeIt = require("scrape-it")
 const urlBTC = process.env.REACT_APP_BTC_URL
 const corsIt = "https://cors.io?"
@@ -12,34 +13,56 @@ const uuid = require("uuid")
 // Set fallback - const url = process.env.REACT_APP_BASE_URL + "/resources"
 // PWA
 
+const amountOfTimeAllowedBeforeScrapedResourcesShouldBeChecked = 1000 * 60 * 60
+
 export const fetchResources = (dispatch, getState) => {
-  scrapeIt(fetchHTMLURL, {
-    item: {
-      listItem: "section div div div ul li",
-      data: {
-        type: {
-          convert: () => "resource"
-        },
-        categoryID: {
-          convert: () => "category_"
-        },
-        _id: {
-          convert: () => `resource_${uuid.v4()}`
-        },
-        titleWithComment: {
-          selector: "a",
-          closest: "li"
-        },
-        title: "a",
-        href: {
-          selector: "a",
-          attr: "href"
+  if (
+    isNil(window.localStorage.getItem("extResources")) ||
+    JSON.parse(window.localStorage.getItem("extResourcesSetTime")) <
+      Date.now() - amountOfTimeAllowedBeforeScrapedResourcesShouldBeChecked
+  ) {
+    scrapeIt(fetchHTMLURL, {
+      item: {
+        listItem: "section div div div ul li",
+        data: {
+          type: {
+            convert: () => "resource"
+          },
+          categoryID: {
+            convert: () => "category_"
+          },
+          _id: {
+            convert: () => `resource_${uuid.v4()}`
+          },
+          titleWithComment: {
+            selector: "a",
+            closest: "li"
+          },
+          title: "a",
+          href: {
+            selector: "a",
+            attr: "href"
+          }
         }
       }
-    }
-  }).then(({ data, response }) => {
-    console.log(`Status Code: ${response.statusCode}`)
-    console.log(data.item)
-    dispatch({ type: GET_RESOURCES, payload: data.item })
-  })
+    }).then(({ data, response }) => {
+      console.log(`Status Code: ${response.statusCode}`)
+      console.log(data.item)
+
+      window.localStorage.setItem("extResources", JSON.stringify(data.item))
+      window.localStorage.setItem(
+        "extResourcesSetTime",
+        JSON.stringify(Date.now())
+      )
+
+      dispatch({ type: GET_RESOURCES, payload: data.item })
+    })
+  } else {
+    const extResources =
+      JSON.parse(window.localStorage.getItem("extResources")) || []
+
+    console.log("Pulled from storage")
+
+    dispatch({ type: GET_RESOURCES, payload: extResources })
+  }
 }
